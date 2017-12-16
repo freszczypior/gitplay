91b830dba62dd8d019e7bf808174fb2b3c742f4d
freszczypior <freszczypior@wp.pl>
2017-12-16 13:42:55 +0100

    Hallo3

--- Hallo.java
+++ Hallo.java
@@ -1,15 +1,16 @@
-class Hello{
+class Hello {
 
     public static void main(String[] args) {
         System.out.println("HAAAAAAAA");
         System.out.println("XXXXXXX");
+        System.out.println("jajo");
 
     }
 
-	void newMethod(){
-}
+    void newMethod() {
+    }
 
-	void bar(){
-}
+    void bar() {
+    }
 
 }
