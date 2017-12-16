75a58baa7eb71fcec32e20b71744b79912b6f786
freszczypior <freszczypior@wp.pl>
2017-12-16 13:52:48 +0100

    najnowsze zmiany

--- Hallo.java
+++ Hallo.java
@@ -1,9 +1,12 @@
 class Hello{
 
+    public Hello() {
+    }
+
     public static void main(String[] args) {
         System.out.println("HAAAAAAAA");
         System.out.println("XXXXXXX");
-
+        System.out.println("super z intellij");
     }
 
 	void newMethod(){
